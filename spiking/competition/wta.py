7b383d6877f4734de6f9c7cc5c68644cb85c8b32
razvan404 <razvan.petec@sectorlabs.ro>
2025-04-10 07:46:04 +0300

    Make the input neurons that not spiked contribute to STDP

--- spiking/competition/wta.py
+++ spiking/competition/wta.py
@@ -5,6 +5,6 @@ from ..utils import choose_random_winner
 
 
 class WinnerTakesAll(CompetitionMechanism):
-    def neurons_to_learn(self, spiking_times: np.ndarray):
+    def neurons_to_learn(self, spiking_times: np.ndarray) -> np.ndarray:
         winner_idx = choose_random_winner(spiking_times)
-        return [winner_idx] if winner_idx is not None else []
+        return np.array([winner_idx]) if winner_idx is not None else np.array([])
--- spiking/learning/stdp.py
+++ spiking/learning/stdp.py
@@ -4,7 +4,14 @@ from spiking.learning.mechanism import LearningMechanism
 
 
 class STDP(LearningMechanism):
-    __slots__ = ["tau_pre", "tau_post", "lr"]
+    __slots__ = [
+        "tau_pre",
+        "tau_post",
+        "lr",
+        "decay_factor",
+        "max_pre_spike_time",
+        "weights_interval",
+    ]
 
     def __init__(
         self,
@@ -12,19 +19,20 @@ class STDP(LearningMechanism):
         tau_post: float = 20,
         lr: float = 0.01,
         decay_factor: float = 1.0,
+        max_pre_spike_time: float = None,
+        weights_interval: (float, float) = (0, 1),
     ):
         """
         Spike-Timing-Dependent Plasticity (STDP).
-
-        Args:
-            tau_pre (float): Time constant for pre-synaptic spike.
-            tau_post (float): Time constant for post-synaptic spike.
-            lr (float): Learning rate for weight updates.
         """
+        if max_pre_spike_time is None:
+            raise ValueError("`max_pre_spike_time` must be provided.")
         self.tau_pre = tau_pre
         self.tau_post = tau_post
         self.lr = lr
         self.decay_factor = decay_factor
+        self.max_pre_spike_time = max_pre_spike_time
+        self.weights_interval = weights_interval
 
     def learning_rate_step(self):
         self.lr *= self.decay_factor
@@ -35,15 +43,17 @@ class STDP(LearningMechanism):
         pre_spike_times: np.ndarray,
         post_spike_times: np.ndarray,
     ) -> np.ndarray:
+        pre_spike_times[pre_spike_times > self.max_pre_spike_time] = (
+            self.max_pre_spike_time
+        )
         delta_t = post_spike_times - pre_spike_times
-        finite_mask = np.isfinite(delta_t)
 
         dw = np.zeros_like(weights)
-        potentiation = finite_mask & (delta_t > 0)
-        depression = finite_mask & (delta_t < 0)
+        potentiation = delta_t > 0
+        depression = delta_t < 0
 
         dw[potentiation] = np.exp(-delta_t[potentiation] / self.tau_pre)
         dw[depression] = -np.exp(delta_t[depression] / self.tau_post)
 
-        updated_weights = np.clip(weights + self.lr * dw, 0, 1)
+        updated_weights = np.clip(weights + self.lr * dw, *self.weights_interval)
         return updated_weights
